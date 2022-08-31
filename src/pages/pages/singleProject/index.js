400e69a44657a1cbb05eb0ec11eb72d5f0cb0983
AdmiralCrunchy <vincentjtate@gmail.com>
2022-08-31 12:43:39 -0700

    pushing single page

---
+++ src/pages/pages/singleProject/index.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+import {useRoute} from 'next/router';
+
+// ** MUI Components
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
+import { styled, useTheme } from '@mui/material/styles'
+import MuiCard from '@mui/material/Card'
+
+const SingleProject = () => {
+
+return (
+    <Box>
+        <CardContent>
+            
+        </CardContent>
+    </Box>
+)
+
+}
+
+export default SingleProject;
