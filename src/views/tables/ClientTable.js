d50063acabb3d7a702b466e13039ee2cd3807452
pem2k <107086485+pem2k@users.noreply.github.com>
2022-09-07 10:40:52 -0700

    invoice page working

--- src/views/tables/ClientTable.js
+++ src/views/tables/ClientTable.js
@@ -28,7 +28,7 @@ const ClientTable = () => {
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-    fetch("http://lancerbackend.herokuapp.com/developers/verify", {
+    fetch("https://lancerbackend.herokuapp.com/developers/verify", {
       method: 'GET', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors',
       contentType: 'application/json',
@@ -53,7 +53,7 @@ const ClientTable = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       
-    fetch("http://lancerbackend.herokuapp.com/developers/home", {
+    fetch("https://lancerbackend.herokuapp.com/developers/home", {
       method: 'GET', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors',
       contentType: 'application/json',
